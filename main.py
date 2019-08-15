26a7e7c55b3328047c4d46cfd8506d0d698c84ff
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2019-08-16 00:30:53 +0300

    new file

---
+++ main.py
@@ -0,0 +1,28 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+ctx=ssl.create_default_context()
+ctx.check_hostname=False
+ctx.verify_mode=ssl.CERT_NONE
+
+
+
+## TODO: add main
+
+url = "https://api.jde.ru/vD/calculator/price?from=1125899906842658&to=1125899906842673&weight=25&width=1&volume=0,02&type=1"
+
+print('Retrieving', url, sep=' ----->>>  ')
+
+#requesting data
+urlop = urllib.request.urlopen(url)
+data = urlop.read().decode()
+print('Retrieved', len(data), 'characters')
+print(data,type(data))
+
+#converting data to .dict
+try:
+    js = json.loads(data)
+except:
+    js = None
+print(js,type(js))
