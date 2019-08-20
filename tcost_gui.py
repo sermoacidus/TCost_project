f65be500d3ed875b05cb458a7270b29a3ceee2a2
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2019-08-20 17:15:25 +0300

    changes for new branch

--- tcost_gui.py
+++ tcost_gui.py
@@ -1,17 +1,26 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import pandas as pd
 
 def count_the_cost():
     if var_for_gp1 == False:
         info_result.configure(text=int(message_from_box_amount1))
 
+class LabelP(tk.Label):
+    def __init__(self,row_inp,column_inp, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.grid(row=row_inp,column=column_inp)
+
+class Combobox_p
+
 
 root = tk.Tk()
 
-gp_label1 = tk.Label(root, text='Аскиз')
-gp_label1.grid(row=1, column=1)
-gp_label2 = tk.Label(root, text='НовокузСорт')
-gp_label2.grid(row=2, column=1)
+df = pd.read_excel('data_set_for_TCost.xlsx')
+for i in range(0,len(df.values)):
+    gp_label = LabelP(i+1, 1, root, text = df.values[i][0])
+
+
 
 
 group_combobox1 = ttk.Combobox(root, values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], height=13)
@@ -35,9 +44,9 @@ box_amount2 = tk.Entry(root, width=3)
 box_amount2.grid(row=2, column=4)
 
 confirm_button = tk.Button(root, text='Рассчитать -->', command=count_the_cost)
-confirm_button.grid(row=3, column=1)
+confirm_button.grid(row=len(df.values)+2, column=1)
 
 info_result = tk.Label(root, text='Стоимость доставки:________')
-info_result.grid(row=3, column=2)
+info_result.grid(row=len(df.values)+2, column=2)
 
 root.mainloop()
