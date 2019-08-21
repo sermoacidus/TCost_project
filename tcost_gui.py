08c8e8d0b1222818a865e1de114c9009fb20b1e4
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2019-08-21 15:34:06 +0300

    done with comboboxes and almost done witch boxes amount input

--- tcost_gui.py
+++ tcost_gui.py
@@ -4,9 +4,11 @@ import pandas as pd
 
 def count_the_cost():
     if var_for_gp1 == False:
-        info_result.configure(text=int(message_from_box_amount1))
-        print(comboboxp.textincombobox.get())
-        print(comboboxp2.textincombobox.get())
+        info_result.configure(text=int(b[1].digitin_entry.get()))
+        print(a[3].textincombobox.get())
+        print(a[4].textincombobox.get())
+        print(a[5].textincombobox.get())
+        #print(comboboxp2.textincombobox.get())
 
 class LabelP(tk.Label):
     def __init__(self,row_inp,column_inp, *args, **kwargs):
@@ -31,6 +33,13 @@ class ComboboxP(ttk.Combobox):
 #def initialize_all_comboboxes():
     #for i in range(0, len(df.values)):
 
+class EntryP(tk.Entry):
+    def __init__(self, row_inp, column_inp, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.grid(row=row_inp, column=column_inp)
+        self.digitin_entry = tk.StringVar()
+        self['textvariable'] = self.digitin_entry
+        self['width'] = 3
 
 root = tk.Tk()
 #root.geometry
@@ -41,13 +50,27 @@ df = pd.read_excel('data_set_for_TCost.xlsx')
 for i in range(0, len(df.values)):
     gp_label = LabelP(i+1, 1, root, text=df.values[i][0])
 
+#кол-во мест
+b = []
+for i in range(0, len(df.values)):
+    b.append('digit_in_entry'+str(i))
+    b[i] = EntryP(i+1, 4, root)
+#message_from_box_amount1 = int()
+#box_amount1 = tk.Entry(root, width=3, textvariable=message_from_box_amount1)
+#box_amount1.grid(row=1, column=4)
+#box_amount2 = tk.Entry(root, width=3)
+#box_amount2.grid(row=2, column=4)
+
+
+
 #Выбор группы грузополучателей по укрупнению (до 13 групп)
 #group_combobox_test.listen_to_text(group_combobox_test_value)
 a = []
 for i in range(0, len(df.values)):
     a.append('group_combobox'+str(i))
     a[i] = ComboboxP(i+1, 2, root)
-print(a[0],type(a[0]),a[1],type(a[1]),a[2],type(a[2]))
+#print(a[0],type(a[0]),a[1],type(a[1]),a[2],type(a[2]))
+
 #print(a)
 #for i in range(0, len(df.values)):
 #    group_combobox = ComboboxP(i+1, 2, root)
@@ -61,16 +84,11 @@ var_for_gp2 = False
 gp_main2 = tk.Checkbutton(root, text='Грузополучатель?', variable=var_for_gp2, onvalue=True, offvalue=False)
 gp_main2.grid(row=2, column=3)
 
-message_from_box_amount1 = int()
-box_amount1 = tk.Entry(root, width=3, textvariable=message_from_box_amount1)
-box_amount1.grid(row=1, column=4)
-box_amount2 = tk.Entry(root, width=3)
-box_amount2.grid(row=2, column=4)
 
 confirm_button = tk.Button(root, text='Рассчитать -->', command=count_the_cost)
 confirm_button.grid(row=len(df.values)+2, column=1)
 
 info_result = tk.Label(root, text='Стоимость доставки:________')
-info_result.grid(row=len(df.values)+2, column=2)
+info_result.grid(row=len(df.values)+2, column=2, columnspan=3)
 
 root.mainloop()
