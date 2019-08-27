9f6c3b5bdac8a586f5e12d81676b1f4674c6311b
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2019-08-27 21:37:57 +0300

    adding scrollbar

--- tcost_gui.py
+++ tcost_gui.py
@@ -53,14 +53,21 @@ class CheckbuttonP(tk.Checkbutton):
         self['onvalue'] = True
         self['offvalue'] = False
 
+def onFrameConfigure(container):
+    container.configure(scrollregion=container.bbox("all"))
+
 root = tk.Tk()
+root.geometry('450x400') #TODO: центрирование
 container = tk.Canvas(root)
-s = ttk.Scrollbar(root, orient=tk.VERTICAL, command=container.yview)
-s.grid(column=1, row=0, sticky=('ns'))
-container['yscrollcommand'] = s.set
-container.configure(yscrollcommand=s.set)
-sometesttext =
-#root.geometry
+main_window = tk.Frame(container)
+scr = ttk.Scrollbar(root, orient="vertical", command=container.yview)
+container.configure(yscrollcommand=scr.set)
+container.pack(side="left", fill="both", expand=True)
+
+scr.pack(side="right", fill="y")
+container.create_window((1, 1), window=main_window, anchor="nw")
+
+main_window.bind("<Configure>", lambda event, container=container: onFrameConfigure(container))
 
 #подгрузка базы грузополучателей
 df = pd.read_excel('data_set_for_TCost.xlsx')
@@ -69,32 +76,32 @@ df = pd.read_excel('data_set_for_TCost.xlsx')
 gp = []
 for i in range(0, len(df.values)):
     gp.append('gp' + str(i))
-    gp[i] = LabelP(i + 1, 1, root, text=df.values[i][0])
+    gp[i] = LabelP(i + 1, 1, main_window, text=df.values[i][0])
     #print(gp[i])
 
 #является ли грузополучателем или же потребитель по укрупнению
 c = []
 for i in range(0, len(df.values)):
     c.append('if'+str(i))
-    c[i] = CheckbuttonP(i+1, 3, root)
+    c[i] = CheckbuttonP(i+1, 3, main_window)
 
 #кол-во мест
 b = []
 for i in range(0, len(df.values)):
     b.append('digit_in_entry'+str(i))
-    b[i] = EntryP(i+1, 4, root)
+    b[i] = EntryP(i+1, 4, main_window)
 
 #Выбор группы грузополучателей по укрупнению (до 13 групп)
 a = []
 for i in range(0, len(df.values)):
     a.append('group_combobox'+str(i))
-    a[i] = ComboboxP(i+1, 2, root)
+    a[i] = ComboboxP(i+1, 2, main_window)
 
 
-confirm_button = tk.Button(root, text='Рассчитать -->', command=count_the_cost)
+confirm_button = tk.Button(main_window, text='Рассчитать -->', command=count_the_cost)
 confirm_button.grid(row=len(df.values)+2, column=1)
 
-info_result = tk.Label(root, text='Стоимость доставки:________')
+info_result = tk.Label(main_window, text='Стоимость доставки:________')
 info_result.grid(row=len(df.values)+2, column=2, columnspan=3)
 
 root.mainloop()
